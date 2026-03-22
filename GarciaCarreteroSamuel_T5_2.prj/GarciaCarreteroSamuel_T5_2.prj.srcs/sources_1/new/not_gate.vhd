d16d479188b9ab8570b1d3bda03cb76a491c8560
paulogomet <pablogomezrobledano@gmail.com>
2026-03-22 18:42:07 +0100

    puertas acabadas

---
+++ GarciaCarreteroSamuel_T5_2.prj/GarciaCarreteroSamuel_T5_2.prj.srcs/sources_1/new/not_gate.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/22/2026 05:45:53 PM
+-- Design Name: 
+-- Module Name: not - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity not_gate is
+--  Port ( );
+    Port (
+        A : in STD_LOGIC;
+        Z : out STD_LOGIC;
+    );
+end not_gate;
+
+architecture Behavioral of not_gate is
+begin
+    Z <= not A;
+end Behavioral;
